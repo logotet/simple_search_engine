8a8fff44a4b6b868f9a9c75844bd26ef81eb649e
logotet <vladimir.vasilev09@gmail.com>
2022-02-15 18:58:46 +0200

    Task 2 completed

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -2,14 +2,51 @@ package search
 
 fun main() {
     print("> ")
-    val words = readLine()!!.split(" ")
-    print("> ")
-    val word = readLine()
-
-    if(words.contains(word)){
+    val numberOfPeople = readLine()!!.toInt()
+    println("Enter all people:")
+    val people = mutableListOf<MutableList<String>>()
+    for (i in 1..numberOfPeople) {
         print("> ")
-        println(words.indexOf(word) + 1)
-    }else{
-        println("Not found")
+        people.add(readLine()!!.split(" ").toMutableList())
     }
+
+    println()
+    println("Enter the number of search queries:")
+    print("> ")
+    val searchQueries = readLine()!!.toInt()
+    println()
+
+    var found = false
+    for (i in 1..searchQueries) {
+        println("Enter data to search people:")
+        print("> ")
+        val query = readLine()
+        val foundResults = mutableListOf<String>()
+        for (line in people) {
+            for(entry in line){
+                val index = entry.toLowerCase().indexOf(query!!.toLowerCase())
+                if(entry.lowercase() == query.lowercase()
+                    || index != -1
+                )
+                    foundResults.add(line.joinToString(" "))
+            }
+//            if (line.contains(query!!.toLowerCase())) {
+//               foundResults.add(line.joinToString(" "))
+//            }
+        }
+        found = !foundResults.isEmpty()
+        if(!found){
+            println("No matching people found.")
+            println()
+        }else {
+            println("People found:")
+            for (row in foundResults.distinct()) {
+                println(row)
+            }
+            println()
+        }
+    }
+
+
+
 }
